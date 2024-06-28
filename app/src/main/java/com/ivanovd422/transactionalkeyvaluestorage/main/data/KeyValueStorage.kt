96134f15adce3ced06cfa0a48cbeac8ae794e084
Denis Ivanov <ivavovd422@gmail.com>
2024-06-28 02:21:34 +0200

    add ReentrantReadWriteLock

--- app/src/main/java/com/ivanovd422/transactionalkeyvaluestorage/main/data/KeyValueStorage.kt
+++ app/src/main/java/com/ivanovd422/transactionalkeyvaluestorage/main/data/KeyValueStorage.kt
@@ -1,6 +1,7 @@
 package com.ivanovd422.transactionalkeyvaluestorage.main.data
 
 import java.util.Stack
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import javax.inject.Inject
 
 class KeyValueStorage @Inject constructor() {
@@ -9,113 +10,152 @@ class KeyValueStorage @Inject constructor() {
     private val valuesCount = mutableMapOf<String, Int>()
     private val transactionStack = Stack<Transaction>()
 
+    private val rwLock = ReentrantReadWriteLock()
+    private val readLock = rwLock.readLock()
+    private val writeLock = rwLock.writeLock()
+
     fun set(key: String, value: String) {
-        if (transactionStack.isEmpty()) {
-            if (storage.containsKey(key) && storage[key] != value) {
-                val oldValue = storage[key]!!
-                if (valuesCount.getOrDefault(oldValue, 0) > 1) {
-                    valuesCount[oldValue] = valuesCount[oldValue]!! - 1
-                } else {
-                    valuesCount.remove(oldValue)
-                }
-            }
-            if (!storage.containsKey(key) || storage[key] != value) {
-                valuesCount[value] = valuesCount.getOrDefault(value, 0) + 1
-            }
-            storage[key] = value
-        } else {
-            transactionStack.peek().apply {
-                if (sessionStorage.containsKey(key) && sessionStorage[key] != value) {
-                    val oldValue = sessionStorage[key]!!
-                    if (sessionValuesCount.getOrDefault(oldValue, 0) > 1) {
-                        sessionValuesCount[oldValue] = sessionValuesCount[oldValue]!! - 1
+        writeLock.lock()
+        try {
+            if (transactionStack.isEmpty()) {
+                if (storage.containsKey(key) && storage[key] != value) {
+                    val oldValue = storage[key]!!
+                    if (valuesCount.getOrDefault(oldValue, 0) > 1) {
+                        valuesCount[oldValue] = valuesCount[oldValue]!! - 1
                     } else {
-                        sessionValuesCount.remove(oldValue)
+                        valuesCount.remove(oldValue)
                     }
                 }
-                if (!sessionStorage.containsKey(key) || sessionStorage[key] != value) {
-                    sessionValuesCount[value] = sessionValuesCount.getOrDefault(value, 0) + 1
+                if (!storage.containsKey(key) || storage[key] != value) {
+                    valuesCount[value] = valuesCount.getOrDefault(value, 0) + 1
                 }
-                sessionStorage[key] = value
+                storage[key] = value
+            } else {
+                transactionStack.peek().apply {
+                    if (sessionStorage.containsKey(key) && sessionStorage[key] != value) {
+                        val oldValue = sessionStorage[key]!!
+                        if (sessionValuesCount.getOrDefault(oldValue, 0) > 1) {
+                            sessionValuesCount[oldValue] = sessionValuesCount[oldValue]!! - 1
+                        } else {
+                            sessionValuesCount.remove(oldValue)
+                        }
+                    }
+                    if (!sessionStorage.containsKey(key) || sessionStorage[key] != value) {
+                        sessionValuesCount[value] = sessionValuesCount.getOrDefault(value, 0) + 1
+                    }
+                    sessionStorage[key] = value
+                }
             }
+        } finally {
+            writeLock.unlock()
         }
     }
 
     fun get(key: String): String? {
-        return if (transactionStack.isEmpty()) {
-            storage[key]
-        } else {
-            transactionStack.peek().sessionStorage[key]
+        readLock.lock()
+        try {
+            return if (transactionStack.isEmpty()) {
+                storage[key]
+            } else {
+                transactionStack.peek().sessionStorage[key]
+            }
+        } finally {
+            readLock.unlock()
         }
     }
 
     fun delete(key: String): String? {
-        if (transactionStack.isEmpty()) {
-            val value = storage[key]
-            if (valuesCount[value]!! > 1) {
-                valuesCount[value!!] = valuesCount[value]!! - 1
-            } else {
-                valuesCount.remove(value)
-            }
-            return storage.remove(key)
-        } else {
-            transactionStack.peek().apply {
-                val value = sessionStorage[key]
-                if (sessionValuesCount[value]!! > 1) {
-                    sessionValuesCount[value!!] = sessionValuesCount[value]!! - 1
+        writeLock.lock()
+        try {
+            if (transactionStack.isEmpty()) {
+                val value = storage[key]
+                if (valuesCount[value]!! > 1) {
+                    valuesCount[value!!] = valuesCount[value]!! - 1
                 } else {
-                    sessionValuesCount.remove(value)
+                    valuesCount.remove(value)
                 }
-                return sessionStorage.remove(key)
+                return storage.remove(key)
+            } else {
+                transactionStack.peek().apply {
+                    val value = sessionStorage[key]
+                    if (sessionValuesCount[value]!! > 1) {
+                        sessionValuesCount[value!!] = sessionValuesCount[value]!! - 1
+                    } else {
+                        sessionValuesCount.remove(value)
+                    }
+                    return sessionStorage.remove(key)
+                }
             }
+        } finally {
+            writeLock.unlock()
         }
     }
 
     fun count(value: String): Int? {
-        return if (transactionStack.isEmpty()) {
-            valuesCount[value]
-        } else {
-            transactionStack.peek().sessionValuesCount[value]
+        readLock.lock()
+        try {
+            return if (transactionStack.isEmpty()) {
+                valuesCount[value]
+            } else {
+                transactionStack.peek().sessionValuesCount[value]
+            }
+        } finally {
+            readLock.unlock()
         }
     }
 
     fun beginTransaction() {
-        if (transactionStack.isEmpty()) {
-            transactionStack.add(Transaction(storage.toMutableMap(), valuesCount.toMutableMap()))
-        } else {
-            val prevTransaction = transactionStack.peek()
-            transactionStack.add(
-                Transaction(
-                    prevTransaction.sessionStorage.toMutableMap(),
-                    prevTransaction.sessionValuesCount.toMutableMap()
+        writeLock.lock()
+        try {
+            if (transactionStack.isEmpty()) {
+                transactionStack.add(Transaction(storage.toMutableMap(), valuesCount.toMutableMap()))
+            } else {
+                val prevTransaction = transactionStack.peek()
+                transactionStack.add(
+                    Transaction(
+                        prevTransaction.sessionStorage.toMutableMap(),
+                        prevTransaction.sessionValuesCount.toMutableMap()
+                    )
                 )
-            )
+            }
+        } finally {
+            writeLock.unlock()
         }
     }
 
     fun commitTransaction() {
-        val transaction = transactionStack.pop()
-        if (transactionStack.isEmpty()) {
-            storage.apply {
-                clear()
-                putAll(transaction.sessionStorage)
+        writeLock.lock()
+        try {
+            val transaction = transactionStack.pop()
+            if (transactionStack.isEmpty()) {
+                storage.apply {
+                    clear()
+                    putAll(transaction.sessionStorage)
+                }
+                valuesCount.apply {
+                    clear()
+                    putAll(transaction.sessionValuesCount)
+                }
+            } else {
+                transactionStack.peek().apply {
+                    sessionStorage.clear()
+                    sessionStorage.putAll(transaction.sessionStorage)
+                    sessionValuesCount.clear()
+                    sessionValuesCount.putAll(transaction.sessionValuesCount)
+                }
             }
-            valuesCount.apply {
-                clear()
-                putAll(transaction.sessionValuesCount)
-            }
-        } else {
-            transactionStack.peek().apply {
-                sessionStorage.clear()
-                sessionStorage.putAll(transaction.sessionStorage)
-                sessionValuesCount.clear()
-                sessionValuesCount.putAll(transaction.sessionValuesCount)
-            }
+        } finally {
+            writeLock.unlock()
         }
     }
 
     fun rollbackTransaction() {
-        transactionStack.pop()
+        writeLock.lock()
+        try {
+            transactionStack.pop()
+        } finally {
+            writeLock.unlock()
+        }
     }
 }
 
